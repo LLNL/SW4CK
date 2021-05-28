9fd50bea25e69cc704e4387887927411bf9497b2
Ramesh Pankajakshan <pankajakshan1@llnl.gov>
2021-05-27 18:34:35 -0700

    Added nevada.cmake. Modified Cmake files

--- host-configs/lassen.cmake
+++ host-configs/lassen.cmake
@@ -1,5 +1,5 @@
 set(ENABLE_CUDA ON CACHE BOOL "")
-# Use Clang compilers for C/C++
+
 set(GCC_VERSION "gcc-8.3.1" CACHE STRING "")
 set(GCC_HOME "/usr/tce/packages/gcc/${GCC_VERSION}")
 
@@ -12,17 +12,15 @@ set(BLT_CXX_STD "c++11" CACHE STRING "")
 #------------------------------------------------------------------------------
 
 
-set(CUDA_TOOLKIT_ROOT_DIR "/usr/tce/packages/cuda/cuda-10.1.243" CACHE PATH "")
+set(CUDA_TOOLKIT_ROOT_DIR "$ENV{CUDA_HOME}" CACHE PATH "")
 set(CMAKE_CUDA_COMPILER "${CUDA_TOOLKIT_ROOT_DIR}/bin/nvcc" CACHE PATH "")
 set(CMAKE_CUDA_HOST_COMPILER "${CMAKE_CXX_COMPILER}" CACHE PATH "")
 
 #set(_cuda_arch "sm_70")
-#set(CMAKE_CUDA_FLAGS "-restrict  -arch ${_cuda_arch} -std=c++11 --expt-extended-lambda " CACHE STRING "")
+set(CMAKE_CUDA_FLAGS "-restrict --expt-extended-lambda " CACHE STRING "")
 
 set(CUDA_SEPARABLE_COMPILATION ON CACHE BOOL "" )
 
 # nvcc does not like gtest's 'pthreads' flag
 set(gtest_disable_pthreads ON CACHE BOOL "")
-
-#set(SW4CK_ENABLE_RAJA ${ENABLE_RAJA})
 
---
+++ host-configs/nevada.cmake
@@ -0,0 +1,7 @@
+set(ENABLE_HIP ON CACHE BOOL "")
+set(HIPCC_VERSION "rocm-4.1.0" CACHE STRING "")
+
+set(CMAKE_CXX_COMPILER "$ENV{ROCM_PATH}/bin/hipcc" CACHE PATH "")
+#set(GPU_TARGETS "gfx908" CACHE STRING "GPU targets to compile for")
+set(SW4_HIP_FLAGS -DENABLE_HIP=1 -isystem $ENV{ROCM_PATH}/hsa/include -isystem $ENV{ROCM_PATH}/hip/include -D__HIP_ARCH_GFX908__=1 -O3 -x hip -Wno-inconsistent-missing-override --amdgpu-target=gfx908  CACHE STRING "")
+
